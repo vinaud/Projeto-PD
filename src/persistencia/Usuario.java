adaf42a5de8e4b7855bdc573b7414b512eb8ba5d
João Vinaud <leyenasd@gmail.com>
2016-03-27 14:05:31 -0300

    Primeiro commit

---
+++ src/persistencia/Usuario.java
@@ -0,0 +1,27 @@
+package persistencia;
+
+public class Usuario {
+
+	private String username;
+	private String senha;
+	private String email;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
