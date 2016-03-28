923f21c0dded5c07a1221b84f896b24c680695f9
João Vinaud <leyenasd@gmail.com>
2016-03-28 16:53:35 -0300

    user

--- src/main/Usuario.java
+++ src/main/Usuario.java
@@ -1,15 +1,58 @@
 package main;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class Usuario {
 
-	public boolean login(String username, String senha)
+	public static boolean login(String username, String senha)
 	{
+		Socket s = null;
+				
+				try {
+					s = new Socket("localhost", 6789);
+					
+					/*conectar com servidor e fazer o login */
+					
+					s.close();
+					return true;
+					
+				} catch (UnknownHostException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		
 		return false;
+		
 		
 	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Socket socket = null;
+		Scanner scan= new Scanner(System.in);
+		
+		System.out.println("Insira o usuário");
+		String user = scan.nextLine();
+		
+		System.out.println("Insira a senha");
+		String senha = scan.nextLine();
+		
+		scan.close();
+		if (login(user,senha))
+		{
 
+			
+		
+		}
+		else
+		{
+			System.out.println("Usuario ou senha invalido");
+		}
+		
 	}
 
 }
