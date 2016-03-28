dc330aa30d266d5bffffad60c21a80930d91075b
João Vinaud <leyenasd@gmail.com>
2016-03-28 14:15:59 -0300

    classes

---
+++ src/arquivos/LeitorDiretorios.java
@@ -0,0 +1,13 @@
+package arquivos;
+
+import java.io.File;
+
+public class LeitorDiretorios {
+ 
+	private File f;
+	
+	public LeitorDiretorios()
+	{
+		//f = new File(null);
+	}
+}
---
+++ src/main/Usuario.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Usuario {
+
+	public boolean login(String username, String senha)
+	{
+		return false;
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
