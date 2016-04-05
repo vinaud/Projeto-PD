a5460293c790440505e9c39dcc64b47b14cfb5f9
João Vinaud <leyenasd@gmail.com>
2016-04-04 23:25:49 -0300

    refatoração no cliente

---
+++ src/conexao/ConexaoCliente.java
@@ -0,0 +1,71 @@
+package conexao;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import main.Cliente;
+
+public abstract class ConexaoCliente {
+
+	
+	public static boolean login(String username, String senha) {
+		Socket socket = null;
+				try {
+					socket = new Socket("localhost", 8787);
+					ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+					Cliente cliente = new Cliente(username, senha);
+					oos.writeObject(cliente);
+					
+					DataInputStream in = new DataInputStream(socket.getInputStream());
+					String resultado = in.readUTF();
+					
+					socket.close();
+					if (resultado.equals("true"))
+					return true;
+					
+				} catch (UnknownHostException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		
+		return false;
+		
+		
+	}
+	
+		public static String execomando(String comando)
+		{
+			Socket socket = null;
+			String resultado = "";
+			try {
+				socket = new Socket("localhost", 8787);
+				
+				DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+				DataInputStream in = new DataInputStream(socket.getInputStream());
+				FileInputStream down;
+				out.writeUTF(comando);
+				resultado = in.readUTF();
+				
+				
+				socket.close();
+				
+				
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return resultado;
+			
+		}
+}
--- src/main/Cliente.java
+++ src/main/Cliente.java
@@ -1,76 +1,16 @@
 package main;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
-import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.Scanner;
+
+import conexao.ConexaoCliente;
 
 public class Cliente {
 
 	public Cliente(String username, String senha) {
-		// TODO Auto-generated constructor stub
+		
 	}
 
-	public static boolean login(String username, String senha) {
-		Socket socket = null;
-				try {
-					socket = new Socket("localhost", 8787);
-					ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-					Cliente cliente = new Cliente(username, senha);
-					oos.writeObject(cliente);
-					
-					DataInputStream in = new DataInputStream(socket.getInputStream());
-					String resultado = in.readUTF();
-					
-					socket.close();
-					if (resultado.equals("true"))
-					return true;
-					
-				} catch (UnknownHostException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-		
-		return false;
-		
-		
-	}
 	
-		public static String execomando(String comando)
-		{
-			Socket socket = null;
-			String resultado = "";
-			try {
-				socket = new Socket("localhost", 8787);
-				
-				DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-				DataInputStream in = new DataInputStream(socket.getInputStream());
-				FileInputStream down;
-				out.writeUTF(comando);
-				resultado = in.readUTF();
-				
-				
-				socket.close();
-				
-				
-			} catch (UnknownHostException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			return resultado;
-			
-		}
 		
 	public static void main(String[] args) {
 		
@@ -78,7 +18,7 @@ public class Cliente {
 		String user ="";
 		String senha ="";
 		
-		while (!login(user,senha))
+		while (!ConexaoCliente.login(user,senha))
 		{
 			System.out.println("Insira o usuario");
 			 user = scan.nextLine();
@@ -101,7 +41,7 @@ public class Cliente {
 			comando = scan.nextLine();
 			
 			String result;
-			result = execomando(comando);
+			result = ConexaoCliente.execomando(comando);
 			
 			System.out.println("Resultado da operação de "+ comando + " : \n" + result);
 			
